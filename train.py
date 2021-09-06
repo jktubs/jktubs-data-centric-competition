2a67d926349868f90282d8da8f8ee1fb9ec3132a
jktubs <>
2021-09-06 16:43:59 +0200

    added tensorboard support as well as confusion matrix calculation

--- train.py
+++ train.py
@@ -3,11 +3,19 @@ from tensorflow import keras
 import numpy as np
 import json
 import sys
+import sklearn
 
-directory = "YOUR_DIRECTORY_NAME_HERE"
-user_data = directory + "/train"
-valid_data = directory + "/val"
-test_data = directory + "/test" # this can be the label book, or any other test set you create
+from datetime import datetime
+from packaging import version
+# spin up tensorboard: tensorboard --logdir logs/scalars
+# https://www.tensorflow.org/tensorboard/scalars_and_keras?hl=en
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
+import io
+
+directory = "modified_data/data"
+test_data = directory + "/label_book"
 
 ### DO NOT MODIFY BELOW THIS LINE, THIS IS THE FIXED MODEL ###
 batch_size = 8
@@ -15,8 +23,15 @@ tf.random.set_seed(123)
 
 
 if __name__ == "__main__":
+    # Returns a datetime object containing the local date and time
+    start_dateTimeObj = datetime.now()
+    print(start_dateTimeObj)
+    logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+    file_writer_cm = tf.summary.create_file_writer(logdir + '/cm')
+
     train = tf.keras.preprocessing.image_dataset_from_directory(
-        user_data + '/train',
+        directory + '/train',
         labels="inferred",
         label_mode="categorical",
         class_names=["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"],
@@ -27,7 +42,7 @@ if __name__ == "__main__":
     )
 
     valid = tf.keras.preprocessing.image_dataset_from_directory(
-        user_data + '/val',
+        directory + '/val',
         labels="inferred",
         label_mode="categorical",
         class_names=["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"],
@@ -86,11 +101,86 @@ if __name__ == "__main__":
         save_weights_only=True,
     )
 
+    def plot_to_image(figure):
+        """Converts the matplotlib plot specified by 'figure' to a PNG image and
+        returns it. The supplied figure is closed and inaccessible after this call.
+        """
+        # Save the plot to a PNG in memory.
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        # Closing the figure prevents it from being displayed directly inside
+        # the notebook.
+        plt.close(figure)
+        buf.seek(0)
+        # Convert PNG buffer to TF image
+        image = tf.image.decode_png(buf.getvalue(), channels=4)
+        # Add the batch dimension
+        image = tf.expand_dims(image, 0)
+        return image
+
+    def plot_confusion_matrix(cm, class_names):
+        """
+        Returns a matplotlib figure containing the plotted confusion matrix.
+
+        Args:
+            cm (array, shape = [n, n]): a confusion matrix of integer classes
+            class_names (array, shape = [n]): String names of the integer classes
+        """
+        figure = plt.figure(figsize=(8, 8))
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+        plt.title("Confusion matrix")
+        plt.colorbar()
+        tick_marks = np.arange(len(class_names))
+        plt.xticks(tick_marks, class_names, rotation=45)
+        plt.yticks(tick_marks, class_names)
+
+        # Compute the labels from the normalized confusion matrix.
+        labels = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+        # Use white text if squares are dark; otherwise black.
+        threshold = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            color = "white" if cm[i, j] > threshold else "black"
+            plt.text(j, i, labels[i, j], horizontalalignment="center", color=color)
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        return figure
+
+    #https://www.tensorflow.org/tensorboard/image_summaries?hl=en
+    def log_confusion_matrix(epoch, logs):
+
+        #test_pred = model.predict(test)
+        #predicted_categories = tf.argmax(test_pred, axis=1)
+
+        #true_categories = tf.concat([y for x, y in test], axis=0)
+
+        #https://stackoverflow.com/questions/64687375/get-labels-from-dataset-when-using-tensorflow-image-dataset-from-directory
+        predictions = np.array([])
+        labels =  np.array([])
+        for x, y in valid:
+            predictions = np.concatenate([predictions, np.argmax(model.predict(x), axis = -1)])
+            labels = np.concatenate([labels, np.argmax(y.numpy(), axis=-1)])
+        class_names = valid.class_names
+
+        # Calculate the confusion matrix.
+        cm = sklearn.metrics.confusion_matrix(labels, predictions)
+        # Log the confusion matrix as an image summary.
+        figure = plot_confusion_matrix(cm, class_names=class_names)
+        cm_image = plot_to_image(figure)
+
+        # Log the confusion matrix as an image summary.
+        with file_writer_cm.as_default():
+            tf.summary.image("Confusion Matrix", cm_image, step=epoch)
+    # Define the per-epoch callback.
+    cm_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_confusion_matrix)
+
     history = model.fit(
         train,
         validation_data=valid,
         epochs=100,
-        callbacks=[checkpoint],
+        callbacks=[checkpoint, tensorboard_callback, cm_callback],
     )
 
     model.load_weights("best_model")
@@ -98,7 +188,32 @@ if __name__ == "__main__":
     loss, acc = model.evaluate(valid)
     print(f"final loss {loss}, final acc {acc}")
 
+    #Testing the model using valid and storing the output in y_pred
+    class_names = valid.class_names
+    print(class_names)
+
     test_loss, test_acc = model.evaluate(test)
     print(f"test loss {test_loss}, test acc {test_acc}")
 
-   
+    predictions = np.array([])
+    labels =  np.array([])
+    for x, y in test:
+        predictions = np.concatenate([predictions, np.argmax(model.predict(x), axis = -1)])
+        labels = np.concatenate([labels, np.argmax(y.numpy(), axis=-1)])
+    class_names = test.class_names
+    # Calculate the confusion matrix.
+    cm = sklearn.metrics.confusion_matrix(labels, predictions)
+    print(cm)
+    # Log the confusion matrix as an image summary.
+    figure = plot_confusion_matrix(cm, class_names=class_names)
+    cm_image = plot_to_image(figure)
+
+    # Log the confusion matrix as an image summary.
+    with file_writer_cm.as_default():
+        tf.summary.image("Confusion Matrix (Test Dataset)", cm_image, step=101)
+
+    # Returns a datetime object containing the local date and time
+    end_dateTimeObj = datetime.now()
+    print(end_dateTimeObj)
+    diff = end_dateTimeObj-start_dateTimeObj
+    print(diff)
